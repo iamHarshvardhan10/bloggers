af8bc348b3a6feb6af83f019272306aabf71af79
Harshvardhan <shreeharshpb11@gmail.com>
2025-08-30 23:47:09 +0530

    completed contact section in home page UI

---
+++ src/components/core/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+const Contact = () => {
+  return (
+    <div className="contact_container w-full h-[20vh] bg-[#262A33] rounded-lg flex flex-col items-center justify-center">
+      <h1 className="text-center text-2xl w-[50%] text-white">
+        Let's create something together!
+      </h1>
+      <div className="btn_container ">
+        <Button className={"rounded-sm bg-[#DE6012] cursor-pointer"}>
+          Contact Us
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/core/Home.jsx
+++ src/components/core/Home.jsx
@@ -8,38 +8,44 @@ import {
   Twitter,
   Youtube,
 } from "lucide-react";
+import Contact from "./Contact";
 
 const Home = () => {
   return (
-    <div className="mt-[50px] bg-[#262A33] home_container rounded-lg shadow-md">
-      <div className="flex items-center justify-between text-white">
-        <Badge
-          variant="secondary"
-          className="bg-blue-500 text-white dark:bg-blue-600 badge cursor-pointer"
-        >
-          <BadgeCheckIcon />
-          Verified
-        </Badge>
-        <div className="flex items-center justify-center gap-2">
-          <Facebook className="hover:text-[#de6012] cursor-pointer" />
-          <Instagram className="hover:text-[#de6012] cursor-pointer" />
-          <Twitter className="hover:text-[#de6012] cursor-pointer" />
-          <Youtube className="hover:text-[#de6012] cursor-pointer" />
+    <>
+      <div className="mt-[50px] bg-[#262A33] home_container rounded-lg shadow-md">
+        <div className="flex items-center justify-between text-white">
+          <Badge
+            variant="secondary"
+            className="bg-blue-500 text-white dark:bg-blue-600 badge cursor-pointer"
+          >
+            <BadgeCheckIcon />
+            Verified
+          </Badge>
+          <div className="flex items-center justify-center gap-2">
+            <Facebook className="hover:text-[#de6012] cursor-pointer" />
+            <Instagram className="hover:text-[#de6012] cursor-pointer" />
+            <Twitter className="hover:text-[#de6012] cursor-pointer" />
+            <Youtube className="hover:text-[#de6012] cursor-pointer" />
+          </div>
+        </div>
+
+        <h1 className="text-4xl text-left w-[100%] text-white font-bold ">
+          Stories That <span className="text-[#DE6012]">Inspire</span> , Ideas
+          That <span className="text-[#DE6012]">Last</span>
+        </h1>
+        <p className="text-xl text-white">
+          Discover thoughtfully written stories, fresh ideas, and perspectives
+          that inspire your day.
+        </p>
+        <div className="btn_container">
+          <Button className={"rounded-sm bg-[#DE6012] cursor-pointer"}>
+            Contact Us
+          </Button>
         </div>
       </div>
-
-      <h1 className="text-4xl text-left w-[100%] text-white font-bold ">
-        Stories That <span className="text-[#DE6012]">Inspire</span> , Ideas
-        That <span className="text-[#DE6012]">Last</span>
-      </h1>
-      <p className="text-xl text-white">
-        Discover thoughtfully written stories, fresh ideas, and perspectives
-        that inspire your day.
-      </p>
-      <div className="btn_container">
-        <Button className={"rounded-sm bg-[#DE6012]"}>Contact Us</Button>
-      </div>
-    </div>
+      <Contact />
+    </>
   );
 };
 
