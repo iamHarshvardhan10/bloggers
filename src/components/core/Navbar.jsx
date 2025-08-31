243064f7f593536615a2d10520297cc04a570ec2
Harshvardhan <shreeharshpb11@gmail.com>
2025-08-31 11:31:10 +0530

    attached rotues for navigation

--- src/components/core/Navbar.jsx
+++ src/components/core/Navbar.jsx
@@ -2,6 +2,7 @@ import { FaHome } from "react-icons/fa";
 import { FaBlog } from "react-icons/fa6";
 import { MdCastConnected } from "react-icons/md";
 import { IoMdLogIn } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -10,10 +11,18 @@ const Navbar = () => {
                     mx-auto mt-[10px] flex items-center justify-center shadow-md relative top-4"
     >
       <div className="flex items-center justify-center gap-10 text-white text-xl">
-        <FaHome className="cursor-pointer hover:text-[#DE6012] transition" />
-        <FaBlog className="cursor-pointer hover:text-[#DE6012] transition" />
-        <MdCastConnected className="cursor-pointer hover:text-[#DE6012] transition" />
-        <IoMdLogIn className="cursor-pointer hover:text-[#DE6012] transition" />
+        <Link to={"/"}>
+          <FaHome className="cursor-pointer hover:text-[#DE6012] transition" />
+        </Link>
+        <Link to={"/blogs"}>
+          <FaBlog className="cursor-pointer hover:text-[#DE6012] transition" />
+        </Link>
+        <Link to={"/contact"}>
+          <MdCastConnected className="cursor-pointer hover:text-[#DE6012] transition" />
+        </Link>
+        <Link to={"/register"}>
+          <IoMdLogIn className="cursor-pointer hover:text-[#DE6012] transition" />
+        </Link>
       </div>
     </nav>
   );
