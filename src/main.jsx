9a448d0272247f22fbd8b2be6fca1ec36a4260bc
Harshvardhan <shreeharshpb11@gmail.com>
2025-09-15 22:26:44 +0530

    added context setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./css/master.scss";
+import { AppContextProvider } from "./context/AppContext";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <AppContextProvider>
     <App />
-  </StrictMode>
+  </AppContextProvider>
 );
